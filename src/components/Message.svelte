14bce39fc0dcf5f8435e71169a67a24bb47d831a
Ashesh Singh <ask@mrsinh.com>
2022-04-11 19:15:46 -0500

    feat: add component for webex messaging

---
+++ src/components/Message.svelte
@@ -0,0 +1,25 @@
+<!--suppress CheckEmptyScriptTag -->
+<script lang="ts">
+  import MicrophoneFilled from 'carbon-icons-svelte/lib/MicrophoneFilled.svelte';
+  import VideoFilled from 'carbon-icons-svelte/lib/VideoFilled.svelte';
+  import Screen from 'carbon-icons-svelte/lib/Screen.svelte';
+  import Close from 'carbon-icons-svelte/lib/Close.svelte';
+  import { onMount } from 'svelte';
+  import { getWebexContext } from '../lib/stores';
+  import { listenMessage } from '../lib/webex';
+
+  export let destination;
+
+  const webexContext = getWebexContext();
+
+  let loading = true;
+
+  onMount(() => {
+    listenMessage($webexContext);
+  });
+</script>
+
+<section class="container centered no-padding" />
+
+<style>
+</style>
